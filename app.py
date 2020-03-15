1b3d7fa749911b11c6979d9fd61f6aa4ce519644
Saisyam <saisyam@saisyam.com>
2020-03-15 11:45:47 +0530

    add github gist feature for a file

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import render_template
+from flask import Markup
+from gitparse import *
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Flask App Done!"
+@app.route('/<path:url>')
+def index(url):
+    filename = url.split("/")[-1]
+    rawurl = url.replace("blob", "raw")
+    document = parsegit(url)
+    return render_template('gistify.html', document=Markup(document), filename=filename, rawurl=rawurl, url=url)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
